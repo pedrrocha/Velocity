958d36af518e270c3390d57fc3d965f3fc424a96
pedrrocha <126675591+pedrrocha@users.noreply.github.com>
2023-11-21 18:25:12 +0000

    Movimentaçao de bala e velocidade

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Shell : MonoBehaviour
 {
     public GameObject explosion;
+    float speed = 0f;   
+    float yspeed = 0f;
+    float mass = 1;
+    float force = 2;
+    float drag = 1;
+    float gravity = 9.8f;
+    float gAceel;
+    float acceleration;
+    
+            
 
     void OnCollisionEnter(Collision col)
     {
@@ -19,12 +29,16 @@ public class Shell : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        acceleration = force/mass;
+        speed += acceleration * 1;
+        gAceel = gravity/mass;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-
+        speed *= (1 - Time.deltaTime * drag);
+        yspeed += gAceel * Time.deltaTime;
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
